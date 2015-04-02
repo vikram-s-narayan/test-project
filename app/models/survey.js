39995b25cbbfd4d56b124b6e7d84a6f93eb37aaf
vikram-s-narayan <vikram@freado.com>
2015-04-02 09:01:09 +0530

    added components for questions

--- app/models/survey.js
+++ app/models/survey.js
@@ -1,16 +1,37 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr('string')
+  name: DS.attr(),
+  type: DS.attr(),
+  placeholder: DS.attr(),
+  action: DS.attr(),
+  cols: DS.attr(),
+  rows: DS.attr()
+
 }).reopenClass({
   FIXTURES: [
-    {
+  /*  {
       id: 1,
       name: 'ram'
     },
     {
       id: 2,
       name: 'rahim'
-    }
+    },
+    {
+      id: 3,
+      name: '<input type="text" placeholder="Name" value=newName action="addName">'
+    },
+*/
+    {
+      id: 4, type: 'text-input', name: 'dinki', placeholder: 'your name', value: 'newName', action: 'addName' },
+    {
+      id: 5, type: 'radio-question', placeholder: 'your name', value: 'newName', action: 'addName' },
+    {
+      id: 6, type: 'textarea-question', placeholder: 'your name', value: 'newName', action: 'addName',
+      cols: "80",
+      rows: "16"
+      }
+
     ]
-    });
+  });
--- app/routes/survey.js
+++ app/routes/survey.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
+//var Handlebars = Ember.Handlebars;
 
 export default Ember.Route.extend({
   model: function(){
+
     return this.store.find('survey');
-    }
-});
+  }
+
+}
+);
